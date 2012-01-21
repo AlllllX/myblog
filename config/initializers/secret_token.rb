2ac87c72b927b0e93d22fd8e3134436bbf2ad7d0
Alex Johnson <zerg4chat@gmail.com>
2012-01-22 00:25:48 +0530

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = 'b100b3dccb0176780ba500fdc26dda8a9d1e31461c2d66e7fc1d5490c586c5105f8b0f0f2fb7c3434ab064c0690bda35f90274ddbc65c346756d6a25ff9a1f42'
